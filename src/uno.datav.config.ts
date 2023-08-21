1d76425a71bc2d8d315c298f16e25bd85e73616b
林岑影 <lincenying@qq.com>
2023-08-21 18:11:55 +0800

    feat: add datav

--- src/uno.datav.config.ts
+++ src/uno.datav.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, presetAttributify, presetIcons, presetUno, transformerDirectives, transformerVariantGroup } from 'unocss'
 import type { Preset } from '@unocss/core'
+import shortcuts from './shortcuts'
 
 const pxRE = /(-?[\.\d]+)px/g
 const remRE = /(-?[\.\d]+)rem/g
@@ -28,18 +29,7 @@ function pxToRemPreset(options: OpType = {}): Preset {
 }
 
 export default (fontSize: number) => defineConfig({
-    shortcuts: [
-        ['btn', 'px-8px py-1px rounded inline-block bg-hex-3aced5ff text-white text-13px cursor-pointer !outline-none hover:bg-hex-3aced5ee disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
-        ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-hex-3aced5dd'],
-        ['flex--c', 'flex items-center'],
-        ['flex-cc', 'flex justify-center items-center'],
-        ['flex-bc', 'flex justify-between items-center'],
-        ['canvas', 'absolute top-0 left-0 w-full h-full'],
-        ['line-1', 'text-truncate'],
-        ['line-2', 'text-truncate line-clamp-2'],
-        ['line-3', 'text-truncate line-clamp-3'],
-        ['line-4', 'text-truncate line-clamp-4'],
-    ],
+    shortcuts,
     presets: [
         /**
          * 默认预设
